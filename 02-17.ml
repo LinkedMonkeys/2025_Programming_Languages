4ee6cc3b9ccc1a113c5d2a95453b7b9d9f5536cf
Jeff Matocha <matochaj@obu.edu>
2025-02-17 12:50:46 -0600

    Refactored code for today's class.

--- 02-17.ml
+++ 02-17.ml
@@ -4,18 +4,18 @@ type boolval =
     | Unknown
 
 (**************************)
-(* Functions for testing. *)
+(* Function for testing. *)
 (**************************)
-(* Given a list of expr lists, build a new list consisting of each of the current lists,
-each with Known true, Known false, and Unknown cons'd on. *)
-let rec add_to_each(xs) =
-    match xs with
-    | [] -> []
-    | x::xs' -> (Known true::x) :: (Known false:: x) :: (Unknown::x) :: add_to_each xs'
-
 (* Generate all lists of length n. *)
 (* int -> (expr list) list *)
 let rec generate n =
+    (* Given a list of expr lists, build a new list consisting of each of the
+    current lists, each with Known true, Known false, and Unknown cons'd on. *)
+    let rec add_to_each(xs) =
+        match xs with
+        | [] -> []
+        | x::xs' -> (Known true::x) :: (Known false:: x) :: (Unknown::x) :: add_to_each xs'
+    in
     match n with
     | 0 -> [[]]
     | _ -> add_to_each(generate(n-1))
