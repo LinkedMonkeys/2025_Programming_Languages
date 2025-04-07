376d740d94cff3567cadb83fbd5496f0bafd9d5c
Jeff Matocha <matochaj@obu.edu>
2025-04-07 14:18:03 -0500

    Our first Ruby program solves AoC 2016 day 1 part A.

---
+++ 04-07/2016day1a.rb
@@ -0,0 +1,29 @@
+input = gets.split(', ')
+
+direction = 0
+locx = 0
+locy = 0
+
+input.each do |s|
+    if s[0] == 'R' then
+        direction = (direction+1) % 4
+    else
+        direction = (direction-1) % 4
+    end
+
+    distance = s[1..].to_i
+    case direction
+    when 0
+        locy += distance
+    when 1
+        locx += distance
+    when 2
+        locy -= distance
+    when 3
+        locx -= distance
+    end
+end
+
+
+puts locx.abs + locy.abs
+
